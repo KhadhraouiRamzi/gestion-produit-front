69d471da563e6162994d6a647a3a07f5db31cf24
KhadhraouiRamzi <KhadhraouiRamzi>
2021-03-09 12:22:32 +0100

    from new Branch

--- src/app/pages/layout/test-mgmnt/test-form/test-form.component.ts
+++ src/app/pages/layout/test-mgmnt/test-form/test-form.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class TestFormComponent implements OnInit {
   radioGroupValue = 'This is value 2';
   form: FormGroup;
+  form2: FormGroup;
 
   constructor(private fb: FormBuilder,) { }
 
@@ -16,8 +17,39 @@ export class TestFormComponent implements OnInit {
     this.form = this.fb.group({
       role: []
     });
+
+    this.form2 = this.fb.group({
+      role: []
+    });
   }
 
-  
+  handleClick(myRadio) {
+    if(myRadio.value== "true")
+    {
+    (document.getElementById("ddlProject")as any).disabled   = false;
+    }
+    else
+    {
+    (document.getElementById("ddlProject") as any).disabled  = true;
+    }
+  }
+
+  updateCheckBox(opts) {
+    var chks = document.getElementsByName("offer");
+
+    if (opts.value == "Del") {
+        for (var i = 0; i <= chks.length - 1; i++) {
+            (chks[i] as any).disabled  = false;
+            document.getElementById("div").innerHTML = 'Checkboxes enabled';
+        }
+    }
+    else {
+        for (var i = 0; i <= chks.length - 1; i++) {
+            (chks[i] as any).disabled  = true;
+            (chks[i] as any).disabled  = false;
+            document.getElementById("div").innerHTML = 'Checkboxes disabled and unchecked';
+        }
+    }
+}
 
 }
